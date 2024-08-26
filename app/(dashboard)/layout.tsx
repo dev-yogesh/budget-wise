1ef9a7bc2a784f77d93af7d9d5ef48905050e663
dev-yogesh <yogeshgupta279@gmail.com>
2024-08-26 08:49:11 +0530

    add: layout and navbar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+import React, { ReactNode } from "react";
+
+function layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="relative flex h-screen w-full flex-col">
+      <div className="w-full">
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default layout;
