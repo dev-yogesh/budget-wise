d964b4f85902bfc30094dfba81b75eff7a46a2eb
dev-yogesh <yogeshgupta279@gmail.com>
2024-10-11 13:34:18 +0530

    add: income, expense and category form and add category api

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
+import CreateTransactionDialog from "./_components/CreateTransactionDialog";
 
 async function page() {
   const user = await currentUser();
@@ -26,19 +27,29 @@ async function page() {
           <p className="text-3xl font-bold">Hello, {user.firstName}! 👋</p>
 
           <div className="flex items-center gap-3">
-            <Button
-              variant={"outline"}
-              className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
-            >
-              New income 🤑
-            </Button>
+            <CreateTransactionDialog
+              trigger={
+                <Button
+                  variant={"outline"}
+                  className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
+                >
+                  New income 🤑
+                </Button>
+              }
+              type="income"
+            />
 
-            <Button
-              variant={"outline"}
-              className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white"
-            >
-              New expense 😤
-            </Button>
+            <CreateTransactionDialog
+              trigger={
+                <Button
+                  variant={"outline"}
+                  className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white"
+                >
+                  New expense 😤
+                </Button>
+              }
+              type="expense"
+            />
           </div>
         </div>
       </div>
